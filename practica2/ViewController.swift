6b69762ee86781dcbd71cebaa7d95004832e1452
Alumne <Alumne@imac-de-apple.local>
2019-04-29 16:18:47 +0200

    Initial Commit

---
+++ practica2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practica2
+//
+//  Created by Alumne on 29/04/2019.
+//  Copyright © 2019 GRV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
