2f8b53a83f3404041e40f005f1c1e4b6e04184b1
glork7 <josepbauza7@gmail.com>
2019-05-13 17:26:13 +0200

    Implemented rotate and pinch gestures, and remplaced long sounds for short sounds, project minimum requirements done. Yay.

--- practica2/ViewController.swift
+++ practica2/ViewController.swift
@@ -13,9 +13,18 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var flute: UIImageView!
+    @IBOutlet var trumpet: UIImageView!
+    @IBOutlet var violin: UIImageView!
+    @IBOutlet var guitar: UIImageView!
+    @IBOutlet var micro: UIImageView!
+    @IBOutlet var drum: UIImageView!
+    
     @IBOutlet var PlayButton: UIBarButtonItem!
+    
     var player:AVAudioPlayer?
-    var instruments:AVAudioPlayer?
+    var instruments:SystemSoundID = 0
+    var pinchGesture = UIPinchGestureRecognizer()
     let audioFile = "harry"
     let audioFile1 = "flute"
     let audioFile2 = "trumpet"
@@ -23,57 +32,6 @@ class ViewController: UIViewController {
     let audioFile4 = "guitar"
     let audioFile5 = "lalala"
     let audioFile6 = "drum"
-    
-    @IBAction func tap(_ sender: UITapGestureRecognizer) {
-        setOffAudio()
-        if let Image1URL = Bundle.main.url(forResource: audioFile1, withExtension: "mp3"){
-            instruments = try! AVAudioPlayer(contentsOf: Image1URL)
-            instruments!.play()
-        }
-        
-    }
-    
-    @IBAction func tap2(_ sender: UITapGestureRecognizer) {
-        setOffAudio()
-        if let Image1URL = Bundle.main.url(forResource: audioFile2, withExtension: "wav"){
-            instruments = try! AVAudioPlayer(contentsOf: Image1URL)
-            instruments!.play()
-        }
-        
-    }
-    
-    @IBAction func tap3(_ sender: UITapGestureRecognizer) {
-        setOffAudio()
-        if let Image1URL = Bundle.main.url(forResource: audioFile3, withExtension: "wav"){
-            instruments = try! AVAudioPlayer(contentsOf: Image1URL)
-            instruments!.play()
-        }
-    }
-    
-    @IBAction func tap4(_ sender: UITapGestureRecognizer) {
-        setOffAudio()
-        if let Image1URL = Bundle.main.url(forResource: audioFile4, withExtension: "wav"){
-            instruments = try! AVAudioPlayer(contentsOf: Image1URL)
-            instruments!.play()
-        }
-    }
-    
-    @IBAction func tap5(_ sender: UITapGestureRecognizer) {
-        setOffAudio()
-        if let Image1URL = Bundle.main.url(forResource: audioFile5, withExtension: "wav"){
-            instruments = try! AVAudioPlayer(contentsOf: Image1URL)
-            instruments!.play()
-        }
-    }
-    
-    @IBAction func tap6(_ sender: UITapGestureRecognizer) {
-        setOffAudio()
-        if let Image1URL = Bundle.main.url(forResource: audioFile6, withExtension: "mp3"){
-            instruments = try! AVAudioPlayer(contentsOf: Image1URL)
-            instruments!.play()
-        }
-    }
-    
     
     override func viewDidLoad(){
         super.viewDidLoad()
@@ -86,16 +44,132 @@ class ViewController: UIViewController {
             print("Error")
         }
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tap(_:)))
-        self.view.addGestureRecognizer(tapGesture)
-        self.view.isUserInteractionEnabled = true
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.pinch(_ :)))
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.pinch2(_ :)))
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.pinch3(_ :)))
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.pinch4(_ :)))
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.pinch5(_ :)))
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.pinch6(_ :)))
+    }
+    
+    @objc func pinchedView(_ sender: UIPinchGestureRecognizer,_ imageView: UIImageView) {
+        switch sender.state{
+        case .changed:
+        self.view.bringSubviewToFront(imageView)
+        sender.view?.transform = (sender.view?.transform)!.scaledBy(x: sender.scale, y: sender.scale)
+        sender.scale = 1.0
+        case .ended:
+        sender.view?.transform = .identity
+        default:
+            break
+        }
+    }
+    
+    @IBAction func pinch(_ sender: UIPinchGestureRecognizer) {
+        pinchedView(sender, flute)
+    }
+    @IBAction func pinch2(_ sender: UIPinchGestureRecognizer) {
+        pinchedView(sender, trumpet)
+    }
+    @IBAction func pinch3(_ sender: UIPinchGestureRecognizer) {
+        pinchedView(sender, violin)
+    }
+    @IBAction func pinch4(_ sender: UIPinchGestureRecognizer) {
+        pinchedView(sender, guitar)
+    }
+    @IBAction func pinch5(_ sender: UIPinchGestureRecognizer) {
+        pinchedView(sender, micro)
+    }
+    @IBAction func pinch6(_ sender: UIPinchGestureRecognizer) {
+        pinchedView(sender, drum)
+    }
+    
+    @objc func rotatedView(_ sender: UIRotationGestureRecognizer,_ imageView: UIImageView) {
+        switch sender.state{
+        case .changed:
+                let animator = UIViewPropertyAnimator(duration: 1, curve: .easeInOut) {
+                    imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                }
+            animator.startAnimation()
+        case .ended:
+            sender.view?.transform = .identity
+        default:
+            break
+            }
         
     }
     
+    @IBAction func rotate(_ sender: UIRotationGestureRecognizer) {
+        rotatedView(sender, flute)
+    }
+    
+    @IBAction func rotate2(_ sender: UIRotationGestureRecognizer) {
+        rotatedView(sender, trumpet)
+    }
+    @IBAction func rotate3(_ sender: UIRotationGestureRecognizer) {
+        rotatedView(sender, violin)
+    }
+    @IBAction func rotate4(_ sender: UIRotationGestureRecognizer) {
+        rotatedView(sender, guitar)
+    }
+    @IBAction func rotate5(_ sender: UIRotationGestureRecognizer) {
+        rotatedView(sender, micro)
+    }
+    @IBAction func rotate6(_ sender: UIRotationGestureRecognizer) {
+        rotatedView(sender, drum)
+    }
+    
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        setOffAudio()
+        if let ImageURL = Bundle.main.url(forResource: audioFile1, withExtension: "mp3"){
+            AudioServicesCreateSystemSoundID(ImageURL as CFURL, &instruments)
+        }
+        AudioServicesPlaySystemSound(instruments)
+        
+    }
+    
+    @IBAction func tap2(_ sender: UITapGestureRecognizer) {
+        setOffAudio()
+        if let ImageURL = Bundle.main.url(forResource: audioFile2, withExtension: "wav"){
+            AudioServicesCreateSystemSoundID(ImageURL as CFURL, &instruments)
+        }
+        AudioServicesPlaySystemSound(instruments)
+        
+    }
+    
+    @IBAction func tap3(_ sender: UITapGestureRecognizer) {
+        setOffAudio()
+        if let ImageURL = Bundle.main.url(forResource: audioFile3, withExtension: "wav"){
+            AudioServicesCreateSystemSoundID(ImageURL as CFURL, &instruments)
+        }
+        AudioServicesPlaySystemSound(instruments)
+    }
+    
+    @IBAction func tap4(_ sender: UITapGestureRecognizer) {
+        setOffAudio()
+        if let ImageURL = Bundle.main.url(forResource: audioFile4, withExtension: "wav"){
+            AudioServicesCreateSystemSoundID(ImageURL as CFURL, &instruments)
+        }
+        AudioServicesPlaySystemSound(instruments)
+    }
+    
+    @IBAction func tap5(_ sender: UITapGestureRecognizer) {
+        setOffAudio()
+        if let ImageURL = Bundle.main.url(forResource: audioFile5, withExtension: "wav"){
+            AudioServicesCreateSystemSoundID(ImageURL as CFURL, &instruments)
+        }
+        AudioServicesPlaySystemSound(instruments)
+    }
+    
+    @IBAction func tap6(_ sender: UITapGestureRecognizer) {
+        setOffAudio()
+        if let ImageURL = Bundle.main.url(forResource: audioFile6, withExtension: "mp3"){
+            AudioServicesCreateSystemSoundID(ImageURL as CFURL, &instruments)
+        }
+        AudioServicesPlaySystemSound(instruments)
+    }
     
     @IBAction func PlayActionButton(_ sender: UIBarButtonItem) {
-        
-        
         if sender.tag == 0 {
             if player!.isPlaying{
                  setOffAudio()
@@ -129,9 +203,5 @@ class ViewController: UIViewController {
         player!.currentTime = 0
         setOffAudio()
     }
-    
-    
-    
-    
 }
 
